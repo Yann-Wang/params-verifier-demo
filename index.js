4c9a37f70c3704a777ee16d9e56608a72070787b
wangyanan <wangyanan@youzan.com>
2018-03-19 00:52:29 +0800

    feat: 搭建es6开发环境

---
+++ index.js
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV === 'development') {
+    const fs = require('fs');
+    const path = require('path');
+  
+    const babelrc = fs.readFileSync(path.join(__dirname, './.babelrc'));
+    let config = {};
+  
+    try {
+      config = JSON.parse(babelrc);
+    } catch (err) {
+      console.error('==> ERROR: Error parsing your .babelrc.');
+      console.error(err);
+    }
+  
+    require('babel-register')(config);
+  }
+
+  require('./demo');
